441e575aa948bdac294e9bdfadd987af1d295eb6
nikagigo <gigoshvilinika53@gmail.com>
2025-02-08 19:59:59 +0400

    Add files via upload

---
+++ level 26/homework/1.py
@@ -0,0 +1,10 @@
+#2)შექმენით სია, რომელშიც მოათავსებთ 8 სხვადასხვა ხილის სახელს "ქართულს".
+#1.დაბეჭდეთ მესამე და მეშვიდე ელემენტები.
+#2.დაბეჭდეთ პირველი ოთხი ელემენტი.
+#3.დაბეჭდეთ ბოლო სამი ელემენტი.
+
+fruits = ["vashli","msxali","atami","mandarini","portoxali","kivi","sazamtro","yurdzeni"]
+print(fruits[2])
+print(fruits[6])
+print(fruits[0:4])
+print(fruits[5:8])
