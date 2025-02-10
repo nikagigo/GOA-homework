2a4257d5d0b92a0bc3d8ba58d583eedf078ba7ce
nikagigo <gigoshvilinika53@gmail.com>
2025-02-10 16:40:32 +0400

    Add files via upload

---
+++ level 28/homework/4.py
@@ -0,0 +1,5 @@
+# 4)შექმენით სია, რომელიც შეიცავს 8 სხვადასხვა ქალაქის სახელებს. წაშალეთ მესამე და მეხუთე ქალაქის სახელი და გამოიტანეთ განახლებული სია.
+city = ["tbilisi","gurjaani","kakabeti","batumi","poti","telavi","rustavi","kachreti"]
+city.pop(2)
+city.pop(3)
+print(city)
